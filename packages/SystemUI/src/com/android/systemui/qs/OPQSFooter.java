541f3204f8ae7a70ae229553e91aa190be96edcc
Panda <jyotiraditya@aospa.co>
2021-02-15 01:11:49 +0530

    SystemUI: Actually fix the new QS footer animation
     * make it fade in and out nicely like QS status header
    
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/OPQSFooter.java
+++ packages/SystemUI/src/com/android/systemui/qs/OPQSFooter.java
@@ -77,6 +77,9 @@ public class OPQSFooter extends LinearLayout {
     private SettingsButton mSettingsButton;
     protected View mEdit;
     protected TouchAnimator mFooterAnimator;
+    protected TouchAnimator mCarrierTextAnimator;
+
+    private SettingsButton mSettingsButton;
     private ActivityStarter mActivityStarter;
     private Boolean mExpanded;
     private Boolean mIsLandscape;
@@ -99,18 +102,21 @@ public class OPQSFooter extends LinearLayout {
         mDataUsageView = findViewById(R.id.data_usage_view);
         mDataUsageView.setVisibility(View.GONE);
         mFooterAnimator = createFooterAnimator();
+        mCarrierTextAnimator = createCarrierTextAnimator();
     }
 
     public void setExpansion(float headerExpansionFraction) {
         if (mFooterAnimator != null) {
             mFooterAnimator.setPosition(headerExpansionFraction);
         }
+        if (mCarrierTextAnimator != null) {
+            mCarrierTextAnimator.setPosition(headerExpansionFraction);
+        }
     }
 
     public void setExpanded(boolean expanded) {
         mExpanded = expanded;
-        if (mCarrierText != null && mDataUsageView != null) {
-            mCarrierText.setVisibility(expanded ? View.GONE : View.VISIBLE);
+        if (mDataUsageView != null) {
             mDataUsageView.setVisibility(expanded ? View.VISIBLE : View.GONE);
             if (expanded) {
                 mDataUsageView.updateUsage();
@@ -125,10 +131,15 @@ public class OPQSFooter extends LinearLayout {
     @Nullable
     private TouchAnimator createFooterAnimator() {
         return new TouchAnimator.Builder()
-                .addFloat(mEdit, "alpha", 0, 1)
-                .addFloat(mDataUsageView, "alpha", 0, 1)
-                .addFloat(mCarrierText, "alpha", 1, 1)
-                .setStartDelay(0.9f)
+                .addFloat(mEdit, "alpha", 0, 0, 1)
+                .addFloat(mDataUsageView, "alpha", 0, 0, 1)
+                .build();
+    }
+
+    @Nullable
+    private TouchAnimator createCarrierTextAnimator() {
+        return new TouchAnimator.Builder()
+                .addFloat(mCarrierText, "alpha", 1, 0, 0)
                 .build();
     }
 
